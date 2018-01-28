67c115730c1856007f155b83f3d5a48eaba0a0cf
Steven Carpenter <stevegcarpenter@gmail.com>
2018-01-28 15:26:59 -0800

    Adding the node object constructor

--- lab-steve/lib/node.js
+++ lab-steve/lib/node.js
@@ -1,7 +1,10 @@
 'use strict';
 
 module.exports = class {
-  constructor(val) {
-
+  constructor(val=null) {
+    if (!val && val !== 0)
+      throw new Error('Error: Val must be a valid piece of data');
+    this.val = val;
+    this.next = null;
   }
 };
